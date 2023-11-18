85991b4d50436783d673f58ab95db31abf1310e6
Mulanga <sikhwethamulanga@gmail.com>
2023-11-18 06:45:38 +0200

    appjsx and pakges

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from "react"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from './pages/Home';
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
 function App() {
-  
-
   return (
-    <>
-     <h1 className="text-red-500">App</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/About" element={<About />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
