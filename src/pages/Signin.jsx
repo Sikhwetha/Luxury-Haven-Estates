40bb1794f55cdfb1e78be6b7457dca6d6b60be12
Mulanga <sikhwethamulanga@gmail.com>
2023-12-21 07:25:18 +0200

    add redux persist

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import { signinStart, signinSuccess, signinFailure } from "../redux/app/user/useSlice";
--- src/redux/app/user/useSlice.js
+++ src/redux/app/user/useSlice.js
@@ -1,32 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
 
 const initialState = {
-    currentUser: null,
-    error: null,
-    isLoading: false,
+  currentUser: null,
+  error: null,
+  isLoading: false,
+};
 
-  }
-
-  const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-      signinStart: (state) => {
-        state.isLoading = true;
-      },
-      signinSuccess: (state, action) => {
-        state.isLoading = false;
-        state.currentUser = action.payload;
-        state.error = null;
-      },
-      signinFailure: (state, action) => {
-        state.error = action.payload;
-        state.isLoading = false;
-      },
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signinStart: (state) => {
+      state.isLoading = true;
+      state.error = null; // Clear any previous errors when starting the sign-in
     },
-  });
+    signinSuccess: (state, action) => {
+      state.isLoading = false;
+      state.currentUser = action.payload;
+      state.error = null;
+    },
+    signinFailure: (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    },
+  },
+});
 
-  export const { signinStart, signinSuccess, signinFailure } = userSlice.actions;
+export const { signinStart, signinSuccess, signinFailure } = userSlice.actions;
 
 export default userSlice.reducer;
-  
